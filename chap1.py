5fd0d06405b405e73fdf495d11b09264fca5c232
mgh3326 <mgh3326@naver.com>
2018-03-24 15:15:18 +0900

    update

--- chap1.py
+++ chap1.py
@@ -108,3 +108,25 @@ knn = KNeighborsClassifier(n_neighbors=1)
 
 
 knn.fit(X_train, y_train)
+
+
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("X_new.shape: {}".format(X_new.shape))
+
+prediction = knn.predict(X_new)
+print("Prediction: {}".format(prediction))
+print("Predicted target name: {}".format(
+iris_dataset['target_names'][prediction]))
+
+y_pred = knn.predict(X_test)
+print("Test set predictions:\n {}".format(y_pred))
+
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
+
+X_train, X_test, y_train, y_test = train_test_split(
+iris_dataset['data'], iris_dataset['target'], random_state=0)
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
