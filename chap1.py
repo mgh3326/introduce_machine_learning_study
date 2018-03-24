23e9894a7e7787c99429f7bf63f33614a473c7df
mgh3326 <mgh3326@naver.com>
2018-03-24 15:11:23 +0900

    update

--- chap1.py
+++ chap1.py
@@ -1,3 +1,4 @@
+# 1장 시작
 import numpy as np
 
 x = np.array([[1, 2, 3], [4, 5, 6]])
@@ -40,3 +41,70 @@ data_pandas = pd.DataFrame(data)
 # IPython.display allows "pretty printing" of dataframes
 # in the Jupyter notebook
 display(data_pandas)
+
+# Select all rows that have an age column greater than 30
+display(data_pandas[data_pandas.Age > 30])
+
+import sys
+print("Python version: {}".format(sys.version))
+import pandas as pd
+print("pandas version: {}".format(pd.__version__))
+import matplotlib
+print("matplotlib version: {}".format(matplotlib.__version__))
+import numpy as np
+print("NumPy version: {}".format(np.__version__))
+import scipy as sp
+print("SciPy version: {}".format(sp.__version__))
+import IPython
+print("IPython version: {}".format(IPython.__version__))
+import sklearn
+print("scikit-learn version: {}".format(sklearn.__version__))
+
+from sklearn.datasets import load_iris
+iris_dataset = load_iris()
+
+print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
+
+print(iris_dataset['DESCR'][:193] + "\n...")
+
+print("Target names: {}".format(iris_dataset['target_names']))
+
+print("Feature names: \n{}".format(iris_dataset['feature_names']))
+
+print("Type of data: {}".format(type(iris_dataset['data'])))
+
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+
+print("First five columns of data:\n{}".format(iris_dataset['data'][:5]))
+
+print("Type of target: {}".format(type(iris_dataset['target'])))
+
+print("Shape of target: {}".format(iris_dataset['target'].shape))
+
+print("Target:\n{}".format(iris_dataset['target']))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+print("X_train shape: {}".format(X_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+
+print("X_test shape: {}".format(X_test.shape))
+print("y_test shape: {}".format(y_test.shape))
+
+# 이거 부터 실행해야됨
+import mglearn
+# create dataframe from data in X_train
+# label the columns using the strings in iris_dataset.feature_names
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+# create a scatter matrix from the dataframe, color by y_train
+grr = pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+
+
+knn.fit(X_train, y_train)
